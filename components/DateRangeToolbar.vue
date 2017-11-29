ffb69b3d260931728349f33b45a5108ea1949758
chaney <ymps3502@gmail.com>
2017-11-29 17:16:27 +0800

    解決路由問題

---
+++ components/DateRangeToolbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-toolbar class="white"
+              flat
+              prominent>
+    <v-toolbar-title class="title grey--text">今天</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-menu :nudge-width="100" class="mr-2">
+      <v-toolbar-title slot="activator"
+                        class="body-2 grey--text">
+        <span>時間</span>
+        <v-icon>arrow_drop_down</v-icon>
+      </v-toolbar-title>
+      <v-list>
+        <v-list-tile v-for="item in time"
+                      :key="item.title"
+                      :to="item.to">
+          <v-list-tile-title v-text="item.title"></v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      timeText: '今天',
+      time: [
+        {
+          title: '今天',
+          to: '/history'
+        },
+        {
+          title: '本週',
+          to: '/history/thisWeek'
+        },
+        {
+          title: '本月',
+          to: '/history/thisMonth'
+        },
+        {
+          title: '今年',
+          to: '/history/thisYear'
+        }
+      ]
+    }
+  }
+  // computeds: {
+  //   TimeText () {
+  //     return $nuxt.$route.path
+  //   }
+  // }
+}
+</script>
+
--- pages/history.vue
+++ pages/history.vue
@@ -1,5 +1,22 @@
 <template>
-  <v-layout>
-    <v-flex>this is history template</v-flex>
-  </v-layout>
+  <v-container fluid
+               grid-list-md>
+    <v-layout row
+              wrap>
+      <v-flex d-flex
+              xs12>
+        <date-range-toolbar></date-range-toolbar>
+      </v-flex>
+      <nuxt-child/>
+    </v-layout>
+  </v-container>
 </template>
+<script>
+import DateRangeToolbar from '../components/DateRangeToolbar'
+export default {
+  components: {
+    DateRangeToolbar
+  }
+}
+</script>
+
