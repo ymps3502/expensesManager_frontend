f1be1bb147f470da5189ca828f45d23a5df05005
chaney <ymps3502@gmail.com>
2018-03-29 14:10:47 +0800

    add margin top 0 class

--- pages/chart.vue
+++ pages/chart.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container fluid
-               grid-list-md>
+               grid-list-md
+               class="mt-0">
     <v-layout row
               wrap>
       <v-flex d-flex
